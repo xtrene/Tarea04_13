fd2f8a712e8322a9825d9769ab69b572a91f16f3
Carlos <carlos@xtrene.com>
2021-06-21 23:27:58 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tarea04_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce tu nombre:  ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Bienvenido/a {0}", nombre);
+
+            Console.ReadKey();
+        }
+    }
+}
